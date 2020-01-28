2b35bdaea14e6c8079b8b1789e9064dd6744ed12
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-01-28 12:02:30 -0500

    new exercises

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask 
 from flask import Flask, flash, redirect, render_template, request,session,abort
 import os
+import sys
 
 app = Flask(__name__)
 
@@ -8,8 +9,9 @@ app = Flask(__name__)
 def home():
     if not session.get('logged_in'):
         return render_template('login.html')
+       
     else:
-       return 'Hello Boss!  <a href="/logout">Logout</a>'
+       return 'Hello Boss!'
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
@@ -23,7 +25,22 @@ def do_admin_login():
 def logout():
     session['logged_in'] = False
     return home()  
-    
+
+@app.route('/example')
+def example():
+    return 'other route'
+
+data = [0,1,2,3,4,5,6]
+
+ for j in data :
+    print (data[j])
+
+@app.route('/num')
+def getAllProducts(data):
+     for i in data:
+         print (data[i])
+         return getAllProducts(data)        
+
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
