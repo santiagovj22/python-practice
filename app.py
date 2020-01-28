d43837f43e5705b3fd6187c6d05e0b8c930b6589
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-01-28 17:19:10 -0500

    get products

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
 from flask import Flask 
 from flask import Flask, flash, redirect, render_template, request,session,abort
-import os
-import sys
+import pyodbc 
 
 app = Flask(__name__)
+# #[ODBC Driver 17 for SQL Server]
+# Description=Microsoft ODBC Driver 17 for SQL Server
+# Driver=/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.4.so.2.1
+# UsageCount=1
+
+conn = pyodbc.connect('DRIVER={PostgreSQL Unicode};SERVER=10.4.28.183;DATABASE=postgres;UID=postgres;PWD=qwe1234*')
+crsr = conn.cursor()
+
+
+def get_product(conn):
+    try:
+        cnxn = conn.cursor()
+        cnxn.execute('select * from products')
+        rows = cnxn.fetchall()
+        return rows
+    except Exception as err:
+        print(err)
 
 @app.route('/')
 def home():
-    if not session.get('logged_in'):
-        return render_template('login.html')
+    retorno = get_product(conn)
+    if retorno : 
+        print(retorno)
+        return 'helo'
     else:
-       return 'Hello Boss!'
+        return 'bye'
+
+    # if not session.get('logged_in'):
+    #     return render_template('login.html')
+    # else:
+    #    return 'Hello Boss!'
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
+    print (request.form)
     if request.form['password'] == 'password' and request.form['username'] == 'admin':
         session['logged_in'] = True
     else:
@@ -42,5 +66,10 @@ for j in data :
 
 
 if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
+  
     app.run(debug=True,host='localhost', port=4000)
+
+
+
+
+
