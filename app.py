57cacd86d01d9e843fe4f0e661c3a7143b6a3fbb
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-01-28 09:50:26 -0500

    add route logout

--- app.py
+++ app.py
@@ -9,18 +9,22 @@ def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return "hablame"
+       return 'Hello Boss!  <a href="/logout">Logout</a>'
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
     if request.form['password'] == 'password' and request.form['username'] == 'admin':
         session['logged_in'] = True
     else:
-            flash('Te equivocaste marditooo')
-    return home()
+     flash('Te equivocaste marditooo')
+     return home()
 
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return home()  
     
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True,host='0.0.0.0', port=4000)
+    app.run(debug=True,host='localhost', port=4000)
