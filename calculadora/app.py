ce0485c8b9c1eac0b89b18c321dd835cb89305d7
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-01-29 16:40:52 -0500

    calculator with basic operations

---
+++ calculadora/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def init() :
+    return 'calculadora'
+
+@app.route('/operaciones', methods = ["GET", "POST"])
+def calculadora():    
+    if request.method == "POST" :
+        num1 = request.form.get("num1")
+        num2 = request.form.get("num2")
+        operador = request.form.get("operador")
+
+        try:
+            resultado = eval(num1 + operador + num2)
+        except:
+
+            return  'no se pudo realizar la operacion'
+
+        return render_template("resultado.html",
+                                num1=num1,
+                                num2=num2,
+                                operador=operador,
+                                resultado=resultado)    
+    else:
+        return render_template("index.html")                            
+
+if __name__ == "__main__" :
+    app.run(host = 'localhost', port = 3000)
