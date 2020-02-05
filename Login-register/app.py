753dc0b0eabe49a9035ea31767d824af4c586aab
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-02-05 07:51:22 -0500

    class register

--- Login-register/app.py
+++ Login-register/app.py
@@ -13,6 +13,12 @@ class LoginForm(FlaskForm):
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max = 80)])
     remember = BooleanField('remember me')
 
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validator=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(),Length(min=4, max = 80)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max = 80)])
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -25,7 +31,8 @@ def login():
 
 @app.route('/signup')
 def signup():
-    return render_template('signup.html')
+    form = RegisterForm()
+    return render_template('signup.html', form = form)
 
 @app.route('/dashboard')
 def dashboard():
